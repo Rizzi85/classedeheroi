e62e48bc523ccf0e924454b2a4ca4fce18a2399d
Rizzi85 <tacrizzi@gmail.com>
2023-11-12 22:43:40 -0300

    primeiro commit

---
+++ ClassedeHeroi.js
@@ -0,0 +1,10 @@
+class heroi {
+    constructor(nome, idade, tipo, ataque){
+        this.nome= nome
+        this.idade= idade
+        this.tipo= tipo
+        this.ataque= ataque
+    }}
+    let alfonso = new heroi("Alfonso",23,"Clérigo","fé")
+    
+    console.log('o '+alfonso.tipo+' atacou usando '+alfonso.ataque)
